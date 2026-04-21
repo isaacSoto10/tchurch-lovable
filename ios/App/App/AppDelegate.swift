9a942c302b31223bf125646c2770dc27700598dc
isaacsotodelarosa123 <isaacsotodelarosa123@hotmail.com>
2026-04-20 22:00:22 -0500

    restore AppDelegate.swift, Base.lproj, Info.plist from main

---
+++ ios/App/App/AppDelegate.swift
@@ -0,0 +1,16 @@
+import UIKit
+import Capacitor
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+        return ApplicationDelegateProxy.shared.application(app, open: url, options: options)
+    }
+}
