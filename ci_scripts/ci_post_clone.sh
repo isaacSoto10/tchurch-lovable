ff02fe14da3e159506bbe2f36ee3a70004f4db3c
isaacsotodelarosa123 <isaacsotodelarosa123@hotmail.com>
2026-05-06 09:24:12 -0500

    Fix Xcode Cloud Capacitor archive resources

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -15,3 +15,8 @@ npm run build
 
 # Sync Capacitor (copies dist into iOS project)
 npx cap sync ios
+
+# Fail early with a clear message if the iOS Capacitor resources are missing.
+test -f ios/App/App/config.xml
+test -f ios/App/App/capacitor.config.json
+test -d ios/App/App/public
