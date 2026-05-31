1833e758056ab7f39130b2306df254423c01908b
isaacsotodelarosa123 <isaacsotodelarosa123@hotmail.com>
2026-05-31 12:04:15 -0500

    Sync devotionals into iOS bundle

---
+++ ios/App/App/public/assets/index-zmmoY0RI.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BP8PXSGR.js","assets/index-BFul0kUH.js","assets/index-DpRbgQH8.css"])))=>i.map(i=>d[i]);
+import{o as r,_ as o}from"./index-BFul0kUH.js";const _=r("Share",{web:()=>o(()=>import("./web-BP8PXSGR.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{_ as Share};
