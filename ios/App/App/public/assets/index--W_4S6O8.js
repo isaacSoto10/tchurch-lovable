a815ea2ffd5aec48ae7ab264322ee696707d5a26
isaacsotodelarosa123 <isaacsotodelarosa123@hotmail.com>
2026-06-08 15:53:49 -0500

    Sync iOS event deep link assets

--- ios/App/App/public/assets/index--W_4S6O8.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BXmdieGm.js","assets/index-DZkupOgJ.js","assets/index-BLcsWvPB.css"])))=>i.map(i=>d[i]);
-import{p as r,_ as t}from"./index-DZkupOgJ.js";const o=r("Share",{web:()=>t(()=>import("./web-BXmdieGm.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
---
+++ ios/App/App/public/assets/index-WndDKKVp.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DOvcJmB7.js","assets/index-_BqIbFMJ.js","assets/index-BLcsWvPB.css"])))=>i.map(i=>d[i]);
+import{p as r,_ as t}from"./index-_BqIbFMJ.js";const o=r("Share",{web:()=>t(()=>import("./web-DOvcJmB7.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
