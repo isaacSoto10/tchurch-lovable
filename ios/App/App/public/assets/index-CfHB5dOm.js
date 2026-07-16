2a939b5a0974cd0ba3f65d3b9f28175bdb4a5b83
isaacsotodelarosa123 <isaacsotodelarosa123@hotmail.com>
2026-07-16 08:56:08 -0500

    build(ios): sync Studio LAN web assets

--- ios/App/App/public/assets/index-CfHB5dOm.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DOadnXuq.js","assets/index-Y-zMci8-.js","assets/index-C-IacRnV.css"])))=>i.map(i=>d[i]);
-import{q as r,a2 as t}from"./index-Y-zMci8-.js";const o=r("Share",{web:()=>t(()=>import("./web-DOadnXuq.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
---
+++ ios/App/App/public/assets/index-CluNrnOs.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CPi9Sw7V.js","assets/index-CiZjDWdX.js","assets/index-D7GFNO5r.css"])))=>i.map(i=>d[i]);
+import{q as e,a2 as o}from"./index-CiZjDWdX.js";const t=e("Browser",{web:()=>o(()=>import("./web-CPi9Sw7V.js"),__vite__mapDeps([0,1,2])).then(r=>new r.BrowserWeb)});export{t as B};
