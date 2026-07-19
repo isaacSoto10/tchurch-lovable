03148f8de9447a28366271b56837714a90efca1d
isaacsotodelarosa123 <isaacsotodelarosa123@hotmail.com>
2026-07-19 13:31:38 -0500

    chore(ios): sync bundled web assets

--- ios/App/App/public/assets/index-C11-aOJZ.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./index-Hs0P3jRs.js";var e=t.createContext(void 0);function i(r){const o=t.useContext(e);return r||o||"ltr"}export{i as u};
---
+++ ios/App/App/public/assets/index-Drnb6XgJ.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-D1wFZAUA.js","assets/index-Bm8KzoA1.js","assets/index-lNlLK1Jm.css"])))=>i.map(i=>d[i]);
+import{q as e,a2 as o}from"./index-Bm8KzoA1.js";const t=e("Browser",{web:()=>o(()=>import("./web-D1wFZAUA.js"),__vite__mapDeps([0,1,2])).then(r=>new r.BrowserWeb)});export{t as B};
