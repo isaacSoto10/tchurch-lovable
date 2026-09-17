13e2f8ba4bc2961adff7ce99eb3f5508ff148690
isaacsotodelarosa123 <isaacsotodelarosa123@hotmail.com>
2026-09-17 06:36:04 -0500

    Sync iOS Capacitor web assets

---
+++ ios/App/App/public/assets/index-s1CU6njM.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DQ6t0INx.js","assets/index-CiQ9XI0t.js","assets/index-CXZFw5Ov.css"])))=>i.map(i=>d[i]);
+import{q as e,a2 as o}from"./index-CiQ9XI0t.js";const t=e("Browser",{web:()=>o(()=>import("./web-DQ6t0INx.js"),__vite__mapDeps([0,1,2])).then(r=>new r.BrowserWeb)});export{t as B};
